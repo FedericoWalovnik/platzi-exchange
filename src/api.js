98c1b7deade9e826de43cb71a540e07625ea16af
Federico <fwalovnik@gmail.com>
2021-02-08 23:47:15 -0300

    added links in each row to send to details

--- src/api.js
+++ src/api.js
@@ -10,6 +10,35 @@ const getAssets = async () => {
   }
 };
 
+const getAsset = async (coin) => {
+  try {
+    const rawData = await fetch(`${baseUrl}/assets/${coin}`);
+    const jsonData = await rawData.json();
+    return jsonData.data;
+  } catch (error) {
+    console.error("The api request was not succesful");
+  }
+};
+
+const getAssetHistory = async (coin) => {
+  try {
+    const now = new Date();
+    const end = now.getTime();
+    now.setDate(now.getDate() - 1);
+    const start = now.getTime();
+
+    const rawData = await fetch(
+      `${baseUrl}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`,
+    );
+    const jsonData = await rawData.json();
+    return jsonData.data;
+  } catch (error) {
+    console.error("The api request was not succesful");
+  }
+};
+
 export default {
-  getAssets
+  getAssets,
+  getAsset,
+  getAssetHistory,
 };
--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -3,10 +3,12 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <px-icon class="mr-2"/>
-          <span class="font-semibold text-xl tracking-tight"
-            >PlatziExchange</span
-          >
+          <px-icon class="mr-2" />
+          <router-link
+            :to="{ name: 'home' }"
+            class="font-semibold text-xl tracking-tight"
+            >PlatziExchange
+          </router-link>
         </div>
         <div
           class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
